a35efebbc5dc398a9dfc8b2e7ef9be0a71bbec41
Bernat Cuixart <bernat.cuixart@estudiantat.upc.edu>
2026-01-30 19:56:50 +0100

    Improved mask behaviour

--- Assets/Scripts/MaskManager.cs
+++ Assets/Scripts/MaskManager.cs
@@ -2,21 +2,43 @@ using UnityEngine;
 
 public class MaskManager : MonoBehaviour
 {
+    public static MaskManager instance;
+
     public bool rMaskOn;
     public bool gMaskOn;
     public bool bMaskOn;
+
+    int masksOn;
+
+    void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
         rMaskOn = false;
         gMaskOn = false;
         bMaskOn = false;
+
+        masksOn = 0;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) rMaskOn = !rMaskOn;
-        if (Input.GetKeyDown(KeyCode.G)) gMaskOn = !gMaskOn;
-        if (Input.GetKeyDown(KeyCode.B)) bMaskOn = !bMaskOn;
+        if (Input.GetKeyDown(KeyCode.R)) { WearOrTakeOffMask(ref rMaskOn); }
+        if (Input.GetKeyDown(KeyCode.G)) { WearOrTakeOffMask(ref gMaskOn); }
+        if (Input.GetKeyDown(KeyCode.B)) { WearOrTakeOffMask(ref bMaskOn); }
+    }
+
+    void WearOrTakeOffMask(ref bool maskOn)
+    {
+        // Can always take it off
+        if (maskOn) { maskOn = false; --masksOn; return; }
+
+        // Cannot wear mask if 2 are already worn
+        if (masksOn >= 2) return;
+        maskOn = true;
+        ++masksOn;
     }
 }
