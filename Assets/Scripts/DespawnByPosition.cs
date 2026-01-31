2c175ce9c1460b7e7b5a94b3c0d0d7041883d448
ChristianBarone <christianxbarone@gmail.com>
2026-01-31 11:54:05 +0100

    Prefabs

---
+++ Assets/Scripts/DespawnByPosition.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DespawnByPosition : MonoBehaviour
+{
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        float bottomLimit = cam.ViewportToWorldPoint(new Vector3(0, -0.2f, 0)).y;
+        if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+            Debug.Log("Despawn");
+        }
+
+    }
+}
