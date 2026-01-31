654bea606cb1c736ede4966811926b5e0a62657f
pcrac18 <polpiquetlasheras@gmail.com>
2026-01-31 19:19:50 +0100

    animacio raw

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitiontime = 1.0f;
+
     public void EmpezarJuego()
     {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel() {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitiontime);
+
         SceneManager.LoadScene("RGBTestScene");
     }
 }
