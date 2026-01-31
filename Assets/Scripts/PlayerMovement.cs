c8cc1933e6a3dca0375b6931d53f0cc7af57e744
Bernat Cuixart <bernat.cuixart@estudiantat.upc.edu>
2026-01-31 11:09:56 +0100

    Improved player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
 
-    float minX, maxX, minY, maxY;
-
     Vector3 movement;
 
     Rigidbody2D rb2D;
     void Start()
     {
-        Camera cam = Camera.main;
         rb2D = GetComponent<Rigidbody2D>();
-
-        Vector3 bottomLeft = cam.ViewportToWorldPoint(new Vector3(0, 0, 0));
-        Vector3 topRight = cam.ViewportToWorldPoint(new Vector3(1, 1, 0));
-
-        float padding = 0.5f;
-
-        minX = bottomLeft.x + padding;
-        maxX = topRight.x - padding;
-        minY = bottomLeft.y + padding;
-        maxY = topRight.y - padding;
     }
 
     void Update()
@@ -31,14 +18,6 @@ public class PlayerMovement : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         movement = new Vector3(h, v, 0) * speed;
-
-        /*
-        transform.position = new Vector3(
-            Mathf.Clamp(transform.position.x, minX, maxX),
-            Mathf.Clamp(transform.position.y, minY, maxY),
-            transform.position.z
-        );
-        */
     }
 
     void FixedUpdate()
