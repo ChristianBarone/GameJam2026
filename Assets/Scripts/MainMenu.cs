2b8761b24464b1f0ef581066e45195a74f431be7
pcrac18 <polpiquetlasheras@gmail.com>
2026-01-31 19:22:27 +0100

    animation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,17 @@ using System.Collections;
 public class MainMenu : MonoBehaviour
 {
     public Animator transition;
-
-    public float transitiontime = 1.0f;
+    public float transitionTime = 1.0f;
 
     public void EmpezarJuego()
     {
-        Time.timeScale = 1f;
+        StartCoroutine(LoadLevel());
+    }
 
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
         SceneManager.LoadScene("RGBTestScene");
     }
 }
